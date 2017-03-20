e2bf7fcf7b269501714c9fcaddffa3789bf31430
Simone Cosma <Simone Cosma>
2017-03-20 23:06:16 +0100

    Add Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule }   from '@angular/router';
 import { AppComponent }  from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 import { HeroService } from './hero.service';
 
@@ -15,15 +16,25 @@ import { HeroService } from './hero.service';
     FormsModule,
     RouterModule.forRoot([
       {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ])
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
     ],
   bootstrap:    [ AppComponent ],
   providers: [HeroService]
